86780dc847eb008c55859e6813430ff4dd2d7ba6
stefano1283474 <stefano.patruno1@gmail.com>
2025-12-07 17:37:27 +0100

    chore(build): set appBuild commit 8c9cabd

--- app.js
+++ app.js
@@ -5071,7 +5071,7 @@ function activateMuroOverride() {
     } catch (_) {}
 }
 try {
-  window.appBuild = { version: '4.3.1', commit: '' };
+  window.appBuild = { version: '4.3.1', commit: '8c9cabd' };
   function renderAppVersion(){
     try {
       var els = document.querySelectorAll('.app-version');
