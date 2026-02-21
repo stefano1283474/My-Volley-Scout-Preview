392c52ce105cb00f793f2662a4a7a40a408007a4
stefano1283474 <stefano.patruno1@gmail.com>
2026-02-21 03:30:31 +0100

    chore: bump version to 12.4.1

--- connection-manager.js
+++ connection-manager.js
@@ -110,7 +110,7 @@ window.safeFirestoreOperation = async (operation, operationName) => {
 console.log('Connection Manager inizializzato');
 
 (function () {
-    window.MVS_APP_VERSION = '12.3.1';
+    window.MVS_APP_VERSION = '12.4.1';
     window.appBuild = window.appBuild || { version: '', commit: '' };
     window.appBuild.version = String(window.MVS_APP_VERSION || '');
     function renderVersion() {
