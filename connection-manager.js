3684f681a11dfc609a8492dac430c13b82be6b17
stefano1283474 <stefano.patruno1@gmail.com>
2026-02-21 02:44:59 +0100

    Update app version to 12.2.1

--- connection-manager.js
+++ connection-manager.js
@@ -110,7 +110,7 @@ window.safeFirestoreOperation = async (operation, operationName) => {
 console.log('Connection Manager inizializzato');
 
 (function () {
-    window.MVS_APP_VERSION = '12.1.1';
+    window.MVS_APP_VERSION = '12.2.1';
     window.appBuild = window.appBuild || { version: '', commit: '' };
     window.appBuild.version = String(window.MVS_APP_VERSION || '');
     function renderVersion() {
