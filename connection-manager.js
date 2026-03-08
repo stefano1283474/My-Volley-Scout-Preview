baae85867f54f98df3622e99f9a4de0ecbb2fb65
stefano1283474 <stefano.patruno1@gmail.com>
2026-03-09 00:11:28 +0100

    release: bump app version to 16.0.1 and align roster UI

--- connection-manager.js
+++ connection-manager.js
@@ -110,7 +110,7 @@ window.safeFirestoreOperation = async (operation, operationName) => {
 console.log('Connection Manager inizializzato');
 
 (function () {
-    window.MVS_APP_VERSION = '15.3.3';
+    window.MVS_APP_VERSION = '16.0.1';
     window.appBuild = window.appBuild || { version: '', commit: '' };
     window.appBuild.version = String(window.MVS_APP_VERSION || '');
     function renderVersion() {
